7bab64c43a814d99f2a6829fcff0f9a2c3475c5b
dzdn504 <enudoko24@gmail.com>
2018-11-01 22:16:36 -0700

    branching and looping

--- hello.go
+++ hello.go
@@ -1,51 +1,17 @@
-// from The Go Programming Language - Pluralsight course
-
 package main
 
 import (
-	"fmt"
+	"github.com/dzdn504/hellogo/greeting"
 )
 
-type Salutation struct {
-	name     string
-	greeting string
-}
-
-type Printer func(string)
-
-func Greet(saluation Salutation, do Printer) {
-	message, alternate := CreateMessage(saluation.name, saluation.greeting, "yo")
-	do(message)
-	do(alternate)
-}
-
-func Print(s string) {
-	fmt.Print(s)
-}
-
-func PrintLine(s string) {
-	fmt.Println(s)
-}
-
-func CreatePrintFunction(custom string) Printer {
-	return func(s string) {
-		fmt.Println(s + custom)
-	}
-}
-
-func PrintCustom(s string, custom string) {
-	fmt.Println(s + custom)
-}
-
-func CreateMessage(name string, greeting ...string) (message string, alternate string) {
-	fmt.Println(len(greeting))
-	message = greeting[1] + " " + name
-	alternate = "HEY!" + " " + name
-	return
-}
-
 func main() {
-	var s = Salutation{"Bob", "Hello"}
+	// var s = greeting.Salutation{"Bob", "Hello"}
 
-	Greet(s, CreatePrintFunction("?"))
+	slice := []greeting.Salutation{
+		{"Bob", "Hello"},
+		{"Mary", "What's goood!?"},
+		{"Joe", "Yo dawg"},
+	}
+
+	greeting.Greet(slice, greeting.CreatePrintFunction("?"), true, 5)
 }

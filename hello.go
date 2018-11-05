33520b66ad14c43025ab216f25632ebe9e9fb444
dzdn504 <enudoko24@gmail.com>
2018-11-05 16:26:48 -0700

    select statements

--- hello.go
+++ hello.go
@@ -60,11 +60,31 @@ func main() {
 
 	fmt.Println("*********************************************")
 	fmt.Fprintf(&salutations[0], "The count is %d", 10)
+
 	c := make(chan greeting.Salutation)
+	c2 := make(chan greeting.Salutation)
+
 	// Call a goroutine that will fill the channel
 	go salutations.ChannelGreeter(c)
+	go salutations.ChannelGreeter(c2)
 
-	for s := range c {
-		fmt.Println(s.Name)
+	for {
+		select {
+		case s, ok := <-c:
+			if ok {
+				fmt.Println(s, ":1")
+			} else {
+				return
+			}
+
+		case s, ok := <-c2:
+			if ok {
+				fmt.Println(s, ":2")
+			} else {
+				return
+			}
+		default:
+			fmt.Println("Waiting...")
+		}
 	}
 }
